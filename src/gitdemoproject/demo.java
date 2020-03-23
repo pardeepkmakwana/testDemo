ad8e2ed71e0ed0c97d2b61f3baebe155e2759425
pardeep <pardeep_wed@hotmail.com>
2020-03-23 08:26:47 -0700

    COMMIT TO BRACHX

--- src/gitdemoproject/demo.java
+++ src/gitdemoproject/demo.java
@@ -3,5 +3,6 @@ package gitdemoproject;
 public class demo {
 public static void main(String args[]){
 	System.out.println("pardeep demo project");
+	System.out.println("suman and aayra also included");
 }
 }
